8e0bf4d4ff01f0a2a22fba9e6942420c2fd5ffab
ayesha nadeem <ayeshanadeem291@gmail.com>
2019-09-30 22:38:11 +0100

    On branch master

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -88,4 +88,58 @@ router.delete('/:id',passport.authenticate('jwt',{session:false}),(req,res)=>{
     })
 });
 
+
+//@route POST api/posts/like/:id
+//@desc like post
+//@access Private
+
+router.post('/like/:id',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    
+    Profile.findOne({user:req.user.id})
+    .then(profile=>{
+        Post.findById(req.params.id)
+        .then(post=>{
+            if(post.likes.filter(like=>like.user.toString()=== req.user.id).length >0)
+{
+    return res.status(400).json({alreadyliked:'user already liked this post'});
+}
+//add user id to likes array
+post.likes.unshift({user:req.user.id});
+post.save().then(post=> res.json(post));
+        
+      })
+        .catch(err =>res.status(404).json({postnotfound:'no post found'}));
+
+    })
+});
+
+//@route POST api/posts/unlike/:id
+//@desc unlike post
+//@access Private
+
+router.post('/unlike/:id',passport.authenticate('jwt',{session:false}),(req,res)=>{
+    
+    Profile.findOne({user:req.user.id})
+    .then(profile=>{
+        Post.findById(req.params.id)
+        .then(post=>{
+            if(post.likes.filter(like=>like.user.toString()=== req.user.id).length === 0)
+{
+    return res.status(400).json({notliked:'you have not liked this post yet'});
+}
+//get the remove index
+const removeIndex = post.likes
+.map(item=>item.user.toString())
+.indexOf(req.user.id);
+//splice it out of array
+post.likes.splice(removeIndex,1);
+//save
+post.save().then(post=>res.json(post));
+        
+      })
+        .catch(err =>res.status(404).json({postnotfound:'no post found'}));
+
+    })
+});
+
 module.exports =router;
