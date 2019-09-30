9f58a7debbecb532570b372079d3bca93fb88fd2
ayesha nadeem <ayeshanadeem291@gmail.com>
2019-09-30 20:00:44 +0100

    On branch master

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -36,4 +36,27 @@ router.post('/',passport.authenticate('jwt',{session:false}),(req,res)=>{
 newPost.save().then(post=>res.json(post));
 });
 
+
+//@route GET api/posts
+//@desc get post
+//@access Public
+
+router.get('/',(req,res)=>{
+    Post.find()
+    .sort({date: -1})
+    .then(posts=>res.json(posts))
+    .catch(err=> res.status(404).json({nopostsfound:'no posts found'}));
+});
+
+//@route GET api/posts/:id
+//@desc get post by id
+//@access Public
+
+router.get('/:id',(req,res)=>{
+    Post.findById(req.params.id)
+    .then(post=>res.json(post))
+    .catch(err=> res.status(404).json({nopostfound:'no post found with that id'}));
+});
+
+
 module.exports =router;
