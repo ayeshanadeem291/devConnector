bff508bc9575b4cf3ff1a41eb256900f70486885
ayesha nadeem <ayeshanadeem291@gmail.com>
2019-10-28 06:02:35 +0000

    On branch master

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -32,35 +32,7 @@ import React, { Component } from 'react'
     </div>
   </nav>
 
- {/* landing */}
-  <div className ="landing">
-    <div className ="dark-overlay landing-inner text-light">
-      <div className ="container">
-        <div className ="row">
-          <div className ="col-md-12 text-center">
-            <h1 className ="display-3 mb-4">Developer Connector
-            </h1>
-            <p className ="lead"> Create a developer profile/portfolio, share posts and get help from other developers</p>
-            <hr />
-            <a href="register.html" className ="btn btn-lg btn-info mr-2">Sign Up</a>
-            <a href="login.html" className ="btn btn-lg btn-light">Login</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  {/* <!-- Footer --> */}
-  <footer className ="bg-dark text-white mt-5 p-4 text-center">
-    Copyright &copy; 2018 Dev Connector
-  </footer>
-
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-    crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-    crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-    crossorigin="anonymous"></script>
+ 
 
 
 
