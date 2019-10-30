bc9b3e9b2c64ab12b10ea3678ae7c3fc70e51690
ayesha nadeem <ayeshanadeem291@gmail.com>
2019-10-30 09:45:43 +0000

    On branch master

--- client/src/actions/authAction.js
+++ client/src/actions/authAction.js
@@ -1,7 +1,8 @@
 //register
 import axios from 'axios';
-import {GET_ERRORS} from './types';
-
+import {GET_ERRORS,SET_CURRENT_USER} from './types';
+import setAuthToken from '../utils/setAuthToken';
+import jwt_decode from 'jwt-decode'
 export const registeruser = (userData,history) => dispatch =>{
 axios
 .post('/api/users/register',userData)
@@ -13,3 +14,34 @@ axios
     })
     );    
 };
+
+//login -get usertoken 
+export const loginUser = (userData)=> dispatch =>{
+   axios.post('/api/users/login',userData)
+   .then(res=> {
+  // save to local storage
+  const {token} = res.data;
+  //set token to localstorage
+  localStorage.setItem('jwtToken',token);
+  //set token to auth header
+  setAuthToken(token);
+  //decode token to get userdata
+  const decoded = jwt_decode(token);
+  //set current user
+  dispatch(setCurrentUser(decoded));
+   })
+   .catch(err => 
+    dispatch({
+        type:GET_ERRORS,
+        payload:err.response.data
+    })
+    )
+};
+
+//set logged in user
+export const setCurrentUser = (decoded) => {
+    return {
+        type :SET_CURRENT_USER,
+        payload:decoded
+    }
+}
